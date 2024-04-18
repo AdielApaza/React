e73d4f87d8a9beaf9d41ce20ee8315568fae474f
AdApzC <161066794+AdielApaza@users.noreply.github.com>
2024-04-18 01:21:50 -0400

    Delete src directory

--- src/Detector.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var elemento = document.querySelector('.status');
-    if (elemento.textContent.includes('Dead')) {
-        elemento.classList.add('destacado');
-    }
-});
